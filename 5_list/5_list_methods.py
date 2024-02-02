1c1ebdfbef74361af24008ba1efad0f3c5ea7540
Andrew <globalshow6@gmail.com>
2024-02-02 22:36:23 +0300

    Добавил примеры для методов

--- 5_list/5_list_methods.py
+++ 5_list/5_list_methods.py
@@ -1,22 +1,87 @@
 # Вопрос: что такое методы?
 
-# Метод append()
+numbers_list = list([40, 20, 90, 11, 5])
+fruits_list = list(['Apple', 'Grape', 'Banan', 'Orange'])
+print("--------------------")
 
+# Метод append() - добавляет элемент в конец списка.
 
-# Метод insert()
+print("append")
+print(numbers_list)
+numbers_list.append(60)
+numbers_list.append("String")
+print(numbers_list)
+print("--------------------")
 
-# Метод pop()
+# Метод pop() - удаляет элемент из списка по его индексу (если нужно удалить последний, то вызываем без ничего)
 
-# Метод remove()
+print("pop")
+print(numbers_list)
+numbers_list.pop()
+print(numbers_list)
+print("--------------------")
 
-# Метод count()
+# Метод insert() - добавляет элемент в список на произвольную позицию (1 аргумент - позиция, 2 аргумент - значение)
 
-# Метод sort()
+print("insert")
+print(numbers_list)
+numbers_list.insert(0, 0)
+print(numbers_list)
+numbers_list.insert(5, 10)
+print(numbers_list)
+print("--------------------")
 
-# Метод clear()
+# Метод remove() - удаляет первый найденный по значению элемент в списке
 
-# Метод index()
+print("remove")
+print(numbers_list)
+numbers_list.remove(40)
+print(numbers_list)
+print("--------------------")
 
-# Метод copy()
+# Метод count() - подсчёт элементов списка по значению
+
+print("count")
+print(numbers_list)
+numbers_list.append(60)
+numbers_list.append(60)
+numbers_list.append(60)
+print(numbers_list)
+print(numbers_list.count(60))
+print("--------------------")
+
+# Метод sort() - сортирует список по возрастанию значений его элементов
+
+print("sort 1 ")
+print(numbers_list)
+numbers_list.sort()
+print(numbers_list) # Какое преимущество нам даёт отсортированный лист, если мы хотим получить конкретное значение ?
+print("--------------------")
+
+# Вопрос: Как происходит сортировка строк?
+print("sort 1 ")
+print(fruits_list)
+fruits_list.sort()
+print(fruits_list)
+print("--------------------")
+
+# Метод reverse() - позволяет перевернуть список
+
+print(numbers_list)
+numbers_list.reverse()
+print(numbers_list)
+print("--------------------")
+
+# Метод clear() - позволяет очистить список
+
+print(numbers_list)
+print(fruits_list)
+
+numbers_list.clear()
+fruits_list.clear()
+
+print(numbers_list)
+print(fruits_list)
+print("--------------------")
 
 # Вопрос: чем отличаются функции от методов?
--- 7_dict/2_dict_methods.py
+++ 7_dict/2_dict_methods.py
@@ -1,11 +1,30 @@
-# Метод get()
+student = dict({'name': 'Andrew', 'age': 22})
+print(student)
+print("--------------------")
 
-# Метод pop()
+# Метод get() - возвращает значение по указанному ключу. Если указанного ключа не существует, метод вернёт None.
 
-# Метод keys()
+print(student.get('name'))
+print(student.get('age'))
+print(student.get('lastname'))
+print("--------------------")
 
-# Метод values()
+# Метод keys() - возвращает специальную коллекцию ключей в словаре dict_keys
 
-# Метод items()
+print(student.keys())
+print(list(student.keys()))
+print("--------------------")
 
-# Метод setdefault()
+# Метод values() - возвращает специальную коллекцию значений в словаре dict_values
+
+print(student.values())
+print(list(student.values()))
+print("--------------------")
+
+# Метод pop() - удаляет ключ и возвращает соответствующее ему значение.
+
+print(student)
+deletedAge = student.pop("age")
+print(deletedAge)
+print(student)
+print("--------------------")
